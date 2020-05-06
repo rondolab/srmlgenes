448c5f9fa5b249b8397d3fb88842c1017a0a69dd
Daniel Jordan <daniel.jordan1@mssm.edu>
2020-05-06 16:47:32 -0400

    Some changes to make_hdf.py CLI for debuggability

--- make_hdf.py
+++ make_hdf.py
@@ -2,17 +2,21 @@ import warnings
 import tables, click
 import numpy as np
 from tqdm import tqdm
+import heatmaps_common
 from heatmaps_common import load_exac_data, load_sim_data, load_filtered_df, LIKELIHOODS, DEMOGRAPHIES, S_VALUES, \
     H_VALUES, FUNCS, GENESETS, LIKELIHOOD_ENUM, DEMOGRAPHY_ENUM, S_ENUM, H_ENUM, FUNC_ENUM, GENESET_ENUM, \
     SimulationHeatmapFixedLength, SimulationHeatmapVariableLength, EmpiricalHeatmap
-from multiprocessing import Pool
+from joblib import Parallel, delayed
 
 
 @click.command()
-@click.option('--output', '-o', type=click.Path(writable=True, dir_okay=False), default='-')
+@click.option('--output', '-o', type=click.Path(writable=True, dir_okay=False), default=heatmaps_common.HEATMAP_TABLES_PATH)
 @click.option('--n-jobs', '-j', type=int, default=1)
 @click.option("--truncate", type=int, default=None)
-def main(output, n_jobs, truncate):
+@click.option("--include-single-sims/--skip-single-sims", default=True)
+@click.option("--include-geneset-sims/--skip-geneset-sims", default=True)
+@click.option("--include-exac/--skip-exac", default=True)
+def main(output, n_jobs, truncate, include_single_sims, include_geneset_sims, include_exac):
     heatmaps_to_load = []
     with tqdm(desc="collecting parameters") as progress:
         for likelihood in LIKELIHOODS:
@@ -22,31 +26,36 @@ def main(output, n_jobs, truncate):
                         for h in H_VALUES:
                             if s == "NEUTRAL" and h != "0.5":
                                 continue
-                            for L in np.arange(2.0, 5.1, 0.1).round(1):
-                                heatmaps_to_load.append(('simulated_single', likelihood, ref_demography, sim_demography, s, h, L))
-                                progress.update(1)
-                            for func in FUNCS:
-                                for geneset in GENESETS:
-                                    for min_L in np.arange(2.0, 5.0, 0.1).round(1):
-                                        for max_L in np.arange(min_L + 0.1, 5.1, 0.1).round(1):
-                                            heatmaps_to_load.append(('simulated_geneset', likelihood, ref_demography, sim_demography, s, h, func, geneset, min_L, max_L))
-                                            progress.update(1)
-                for func in FUNCS:
-                    for geneset in GENESETS:
-                        for min_L in np.arange(0.0, 6.0, 0.1).round(1):
-                            for max_L in np.arange(min_L + 0.1, 6.1, 0.1).round(1):
-                                heatmaps_to_load.append(('exac', likelihood, ref_demography, func, geneset, min_L, max_L))
-                                progress.update(1)
+                            if include_single_sims:
+                                for L in np.arange(2.0, 5.1, 0.1).round(1):
+                                    if include_single_sims:
+                                        heatmaps_to_load.append(('simulated_single', likelihood, ref_demography, sim_demography, s, h, L))
+                                        progress.update(1)
+                            if include_geneset_sims:
+                                for func in FUNCS:
+                                    for geneset in GENESETS:
+                                        for min_L in np.arange(2.0, 5.0, 0.1).round(1):
+                                            for max_L in np.arange(min_L + 0.1, 5.1, 0.1).round(1):
+                                                heatmaps_to_load.append(('simulated_geneset', likelihood, ref_demography, sim_demography, s, h, func, geneset, min_L, max_L))
+                                                progress.update(1)
+                if include_exac:
+                    for func in FUNCS:
+                        for geneset in GENESETS:
+                            for min_L in np.arange(0.0, 6.0, 0.1).round(1):
+                                for max_L in np.arange(min_L + 0.1, 6.1, 0.1).round(1):
+                                    heatmaps_to_load.append(('exac', likelihood, ref_demography, func, geneset, min_L, max_L))
+                                    progress.update(1)
     if truncate is not None:
         heatmaps_to_load = heatmaps_to_load[:truncate]
+    if output == heatmaps_common.HEATMAP_TABLES_PATH:
+        heatmaps_common.HEATMAP_TABLES_FILE.close()
     with tables.open_file(output, mode='w', title='heatmaps', filters=tables.Filters(complevel=6, complib="blosc")) as h5file:
         heatmaps_group = h5file.create_group(h5file.root, "heatmaps")
         h5file.create_table(heatmaps_group, "simulated_single", SimulationHeatmapFixedLength, expectedrows=15000)
         h5file.create_table(heatmaps_group, "simulated_geneset", SimulationHeatmapVariableLength, expectedrows=3000000)
         h5file.create_table(heatmaps_group, "exac", EmpiricalHeatmap, expectedrows=50000)
-        with Pool(n_jobs) as pool:
-            for table_name, row in tqdm(pool.imap(load_heatmap, heatmaps_to_load), total=len(heatmaps_to_load), desc="calculating heatmaps"):
-                getattr(heatmaps_group, table_name).append([row])
+        for table_name, row in Parallel(n_jobs=n_jobs)(delayed(load_heatmap)(*args) for args in tqdm(heatmaps_to_load, desc="calculating heatmaps")):
+            getattr(heatmaps_group, table_name).append([row])
         # index tables
         for col_name in tqdm(["likelihood", "ref_demography", "sim_demography", "s", "h", "L"],
                              desc="indexing simulated_single table"):
@@ -61,8 +70,7 @@ def main(output, n_jobs, truncate):
             heatmaps_group.exac.col(col_name).create_index()
 
 
-def load_heatmap(packed_args):
-    kind, *args = packed_args
+def load_heatmap(kind, *args):
     if kind == "simulated_single":
         likelihood, ref, sim, s, h, L = args
         record = (LIKELIHOOD_ENUM[likelihood],
