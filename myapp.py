d35fb700d6316f201d95d04e6b0deb2a811c2406
Daniel Jordan <daniel.jordan1@mssm.edu>
2021-05-02 21:00:55 -0400

    added app name

--- myapp.py
+++ myapp.py
@@ -22,4 +22,5 @@ def create_app(app_name, app_filename, development_mode=False):
     server.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
     app = dash.Dash(__name__, server=server, external_stylesheets=external_stylesheets,
                     requests_pathname_prefix=quote(str(app_url) + "/"))
+    app.title = "srMLGenes"
     return app
